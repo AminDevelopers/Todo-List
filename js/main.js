ee13d1c1a3ac9c3b02f431a7a91d84a5559c9dec
amin <aminledragon@gmail.com>
2023-12-05 09:33:15 +0100

    set commentary in english

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-//  creation des variables
+//  creating basics variables
 let taskInput = document.querySelector("#taskInput");
 let btnTask = document.querySelector("#addTaskBtn");
 let taskList = document.querySelector("#taskList");
 
 btnTask.addEventListener("click", () => {
-  // creation de variable pour recuperer la valuer de l'input sans espacement
+  // creating variable for having the value of the input without spacement
   let taskInputText = taskInput.value.trim();
-  // condition if si l'input n'est pas vide dans ce cas creation de li, append, et vider l'input
+  // condition if input is not empty, in that case creating the li, append, and clear the input
   if (taskInputText !== "") {
     let taskLi = document.createElement("li");
     taskList.appendChild(taskLi);
     taskInput.value = "";
   }
+
+  // adding eventlistener for marking tasks as completed
 });
